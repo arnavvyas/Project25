5c1aad61e5c844ff81549ab29a481de4709466bc
arnavvyas <77038050+arnavvyas@users.noreply.github.com>
2021-06-11 14:42:38 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,10 @@
+class Paper{
+    costructor(x,y,w,h){
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2
+        }
+    }
+}
